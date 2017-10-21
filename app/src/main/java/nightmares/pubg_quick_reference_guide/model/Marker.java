f82669ca017ca9ca8eec188abfcb66c07496fe75
mjuar005 <mjuar005@fiu.edu>
2017-10-20 23:59:21 -0400

    updated json

---
+++ app/src/main/java/nightmares/pubg_quick_reference_guide/model/Marker.java
@@ -0,0 +1,10 @@
+package nightmares.pubg_quick_reference_guide.model;
+
+/**
+ * Created by user on 10/20/2017.
+ */
+
+public class Marker {
+    long x
+
+}
--- app/src/main/java/nightmares/pubg_quick_reference_guide/presenter/PubgMarkerPresenter.java
+++ app/src/main/java/nightmares/pubg_quick_reference_guide/presenter/PubgMarkerPresenter.java
@@ -1,8 +1,19 @@
 package nightmares.pubg_quick_reference_guide.presenter;
 
+import android.os.AsyncTask;
+
 /**
  * Created by user on 10/10/2017.
  */
 
-public class PubgMarkerPresenter {
+public class PubgMarkerPresenter extends AsyncTask {
+
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+
+    protected void onPostExecute(Long result) {
+
+    }
 }
